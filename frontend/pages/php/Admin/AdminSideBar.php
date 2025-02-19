cc48ccdc08a7cded28fac3ba1fbbc88ede4592a5
Magage Iresh Eranga Magage <iresheranga2020al@gmail.com>
2025-02-19 09:33:34 +0530

    Create AdminSideBar.php

---
+++ frontend/pages/php/Admin/AdminSideBar.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="../../../css/Admin/SideBar.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="logo">
+            <i class="fas fa-dumbbell"></i> <span>Gym Admin</span>
+        </div>
+        <ul class="nav-links">
+            <li class="active">
+                <a href="#"><img src="../../../assets/icons/user.png" alt=""> Home</a>
+            </li>
+            <li>
+                <a href="#"><i class="fas fa-chart-line"></i> Memberships</a>
+            </li>
+            <li>
+                <a href="#"><i class="fas fa-shopping-cart"></i> Trainers</a>
+            </li>
+            <li>
+                <a href="#"><img src="../../../assets/icons/user.png" alt="members"> Members</a>
+            </li>
+            <li>
+                <a href="#"><i class="fas fa-users"></i> Payments</a>
+            </li>
+        </ul>
+        <div class="user-profile">
+            <img src="../../../assets/images/admin.png" alt="Admin">
+            <span>Admin</span>
+            <i class="fas fa-chevron-down"></i>
+        </div>
+    </div>
+</body>
+</html>
