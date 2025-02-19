701e993d54082723dadcf5ca09656d0332acdea8
Magage Iresh Eranga Magage <iresheranga2020al@gmail.com>
2025-02-19 10:23:06 +0530

    Update AdminMember.php

--- frontend/pages/php/Admin/Members/AdminMember.php
+++ frontend/pages/php/Admin/Members/AdminMember.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['user_id'])) {
 <head>
     <meta charset="UTF-8">
     <title>Admin Dashboard - Members</title>
-    <link rel="stylesheet" href="../../../../css/Admin/AdminHome.css"> 
+    <link rel="stylesheet" href="../../../../css/Admin/Members/AdminMember.css"> 
     <link rel="stylesheet" href="../../../../css/Admin/SideBar.css"> 
 </head>
 <body>
@@ -23,19 +23,19 @@ if (!isset($_SESSION['user_id'])) {
         </div>
         <ul class="nav-links">
             <li >
-                <a href="#"><img src="../../../assets/icons/user.png" alt=""> Home</a>
+                <a href="#"><img src="../../../../assets/icons/home.png" alt="" style="width: 20px; height: 20px; margin-right: 10px; vertical-align: middle;"> Home</a>
             </li>
             <li>
-                <a href="#"><i class="fas fa-chart-line"></i> Memberships</a>
+                <a href="#"><img src="../../../assets/icons/user.png" alt="" style="width: 20px; height: 20px; margin-right: 10px; vertical-align: middle;"> Memberships</a>
             </li>
             <li>
-                <a href="#"><i class="fas fa-shopping-cart"></i> Trainers</a>
+                <a href="#"><img src="../../../assets/icons/user.png" alt="" style="width: 20px; height: 20px; margin-right: 10px; vertical-align: middle;"> Trainers</a>
             </li>
             <li class="active">
-                <a href="#"><img src="../../../../assets/icons/user.png" alt="members"> Members</a>
+                <a href="#"><img src="../../../../assets/icons/user.png" alt="members" style="width: 20px; height: 20px; margin-right: 10px; vertical-align: middle;"> Members</a>
             </li>
             <li>
-                <a href="#"><i class="fas fa-users"></i> Payments</a>
+                <a href="#"><img src="../../../assets/icons/user.png" alt="" style="width: 20px; height: 20px; margin-right: 10px; vertical-align: middle;"> Payments</a>
             </li>
         </ul>
         <div class="user-profile">
@@ -47,61 +47,8 @@ if (!isset($_SESSION['user_id'])) {
 
     <!-- Main Content -->
     <div class="main-content">
-        <h1>Welcome Admindfg</h1>
+        <h1>Members</h1>
 
-        <!-- Admin Cards -->
-        <div class="adminCards">
-            <div class="card">
-                <div class="icon">
-                    <img src="../../../assets/gif/user.gif" alt="Members Icon">
-                </div>
-                <h2>Total Members</h2>
-                <p>250</p>
-            </div>
-
-            <div class="card">
-                <div class="icon">
-                    <img src="../../../assets/icons/workouts.svg" alt="Workouts Icon">
-                </div>
-                <h2>Active Workouts</h2>
-                <p>45</p>
-            </div>
-
-            <div class="card">
-                <div class="icon">
-                    <img src="../../../assets/icons/revenue.svg" alt="Revenue Icon">
-                </div>
-                <h2>Revenue</h2>
-                <p>$12,500</p>
-            </div>
-        </div>
-
-        <!-- Second Row -->
-        <div class="adminCards">
-            <div class="card">
-                <div class="icon">
-                    <img src="../../../assets/icons/trainers.svg" alt="Trainers Icon">
-                </div>
-                <h2>Trainers</h2>
-                <p>10</p>
-            </div>
-
-            <div class="card">
-                <div class="icon">
-                    <img src="../../../assets/icons/subscriptions.svg" alt="Subscriptions Icon">
-                </div>
-                <h2>New Subscriptions</h2>
-                <p>120</p>
-            </div>
-
-            <div class="card">
-                <div class="icon">
-                    <img src="../../../assets/icons/sessions.svg" alt="Sessions Icon">
-                </div>
-                <h2>Upcoming Sessions</h2>
-                <p>30</p>
-            </div>
-        </div>
     </div>
 </body>
 </html>
